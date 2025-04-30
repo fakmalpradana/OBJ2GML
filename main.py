8bcea21ff25e60e3670e166efc8c81103ec574ba
Fairuz Akmal Pradana <fakmalpradana@gmail.com>
2025-04-30 14:33:47 +0700

    modify translation input

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ nlp = "AG_09"
 sub_grid = "A"
 bo = "percepatan/OBJ/AG_09/AG_09_A/AG-09-A_BO_Caesar Yoga_BUFFER_Lengkap.geojson"
 
+tx = 692827.46065
+ty = 9326588.60235
+
 sp.call([
     "go", "run", "objseparator.go", 
+    "-cx", f"{tx}", "-cy", f"{ty}",
     f"percepatan/OBJ/{nlp}/{nlp}_{sub_grid}/{nlp}_{sub_grid}.obj", 
     f"percepatan/OBJ/{nlp}/{nlp}_{sub_grid}/AG-09-A_BO_Caesar Yoga_BUFFER_Lengkap.geojson"
 ])
@@ -16,8 +20,8 @@ sp.call([
 sp.call([
     "go", "run", "translate.go", 
     f"-input=export/{nlp}_{sub_grid}.obj", 
-    "-tx=692827.46065", 
-    "-ty=9326588.60235",
+    f"-tx={tx}", 
+    f"-ty={ty}",
     "-tz=100"
 ])
 
