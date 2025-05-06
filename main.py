2b65500a1d173a3e979d306c1ef5620e1684efc4
Fairuz Akmal Pradana <fakmalpradana@gmail.com>
2025-05-06 11:24:15 +0700

    modify main

--- main.py
+++ main.py
@@ -3,34 +3,38 @@ import time
 
 start = time.time()
 
-nlp = "AG_09"
+nlp = "AN_13"
 sub_grid = "A"
-bo = "percepatan/OBJ/AG_09/AG_09_A/AG-09-A_BO_Caesar Yoga_BUFFER_Lengkap.geojson"
+bo = "percepatan/OBJ/AN_13/AN_13_A/AN-13-A-BO.geojson"
 
-tx = 692827.46065
-ty = 9326588.60235
+tx = 696436.71322
+ty = 9320053.52220
 
+# Pemisahan Bangunan
 sp.call([
     "go", "run", "objseparator.go", 
-    "-cx", f"{tx}", "-cy", f"{ty}",
+    f"-cx={tx}", f"-cy={ty}",
     f"percepatan/OBJ/{nlp}/{nlp}_{sub_grid}/{nlp}_{sub_grid}.obj", 
-    f"percepatan/OBJ/{nlp}/{nlp}_{sub_grid}/AG-09-A_BO_Caesar Yoga_BUFFER_Lengkap.geojson"
+    f"{bo}"
 ])
 
+# Translasi Objek Menuju Koordinat UTM
 sp.call([
     "go", "run", "translate.go", 
     f"-input=export/{nlp}_{sub_grid}.obj", 
     f"-tx={tx}", 
     f"-ty={ty}",
-    "-tz=100"
+    "-tz=0"
 ])
 
+# Convert OBJ ke CityGML
 sp.call([
     "go", "run", "obj2gml.go",
     "-input", f"export/{nlp}_{sub_grid}.obj_translated",
     "-output", f"export/{nlp}_{sub_grid}.obj_translated_gml"
 ])
 
+# Merge Keseluruhan CityGML file menjadi 1 file
 sp.call([
     "go", "run", "mergegml.go",
     "-input", f"export/{nlp}_{sub_grid}.obj_translated_gml",
