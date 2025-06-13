146a3b8f99d5072069a78f0e9db8bb9df82c7332
Fairuz Akmal Pradana <fakmalpradana@gmail.com>
2025-06-14 01:21:20 +0800

    setup for batch process

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import time
 import os
 import shutil
 
+from findFile import find_complete_sets, read_and_convert_txt
+from pathlib import Path
+
 start = time.time()
 
-nlp = "AG_09"
-sub_grid = "C"
-bo = "percepatan/OBJ/AG_09/AG_09_C/AG-09-C_BO_Caesar Yoga_BUFFER_Lengkap.geojson"
+root_dir = "percepatan_new/OBJ/2025_06_13"
 
-tx = 692542.174723411328159
-ty = 9326588.18167
+file_set = find_complete_sets(root_dir)
 
 def delete_files(directory):
     files_to_delete = ["12030.obj", "12030.mtl", "12030.gml"]
@@ -37,68 +37,83 @@ def delete_directories(directories):
         else:
             print(f"Directory not found or not a directory: {directory}")
 
-# Pemisahan Bangunan
-sp.call([
-    "go", "run", "objseparator.go", 
-    f"-cx={tx}", f"-cy={ty}",
-    f"percepatan/OBJ/{nlp}/{nlp}_{sub_grid}/{nlp}_{sub_grid}.obj", 
-    f"{bo}"
-])
+for i in file_set:
+    obj = i[0]
+    coord = read_and_convert_txt(i[1])
+    bo = i[2]
 
-# Translasi Objek Menuju Koordinat UTM
-sp.call([
-    "go", "run", "translate.go", 
-    f"-input=export/{nlp}_{sub_grid}.obj", 
-    f"-tx={tx}", 
-    f"-ty={ty}",
-    "-tz=0"
-])
+    root_path = Path(root_dir)
+    obj_path = Path(obj)
 
-# Generate MTL
-sp.call([
-    "python", "semantic_mapping.py",
-    "--obj-dir", f"export/{nlp}_{sub_grid}.obj_translated",
-    "--geojson", f"{bo}"
-])
-delete_files(f"export/{nlp}_{sub_grid}.obj_translated")
+    rel_path = obj_path.relative_to(root_path)
+    folder_name = rel_path.parts[0]
+    print(folder_name)
 
-# Convert OBJ ke CityGML lod2
-sp.call([
-    "go", "run", "obj2lod2gml.go",
-    "-input", f"export/{nlp}_{sub_grid}.obj_translated",
-    "-output", f"export/{nlp}_{sub_grid}.obj_translated_gml"
-])
-delete_files(f"export/{nlp}_{sub_grid}.obj_translated_gml")
+    output_path = f"{root_dir}/{folder_name}.gml".replace('OBJ', 'CityGML')
+    os.makedirs(f"{root_dir}".replace('OBJ', 'CityGML'), exist_ok=True)
 
-# # Convert OBJ ke CityGML lod1
-# sp.call([
-#     "go", "run", "obj2gml.go",
-#     "-input", f"export/{nlp}_{sub_grid}.obj_translated",
-#     "-output", f"export/{nlp}_{sub_grid}.obj_translated_gml"
-# ])
+    # Pemisahan Bangunan
+    sp.call([
+        "go", "run", "objseparator.go", 
+        f"-cx={coord[0]}", f"-cy={coord[1]}",
+        f"{obj}", 
+        f"{bo}"
+    ])
 
-# Merge keseluruhan CityGMl lod2 file menjadi 1 file
-sp.call([
-    "python", "lod2merge.py",
-    f"export/{nlp}_{sub_grid}.obj_translated_gml",
-    f"percepatan/citygml/{nlp}_{sub_grid}.gml",
-    "--name", f"{nlp}_{sub_grid}"
-])
+    # Translasi Objek Menuju Koordinat UTM
+    sp.call([
+        "go", "run", "translate.go", 
+        f"-input=export/{rel_path.parts[1]}", 
+        f"-tx={coord[0]}", 
+        f"-ty={coord[1]}",
+        "-tz=0"
+    ])
 
-# # Merge Keseluruhan CityGML lod1 file menjadi 1 file
-# sp.call([
-#     "go", "run", "mergegml.go",
-#     "-input", f"export/{nlp}_{sub_grid}.obj_translated_gml",
-#     "-output", f"percepatan/citygml/{nlp}_{sub_grid}.gml"
-# ])
+    # Generate MTL
+    sp.call([
+        "python", "semantic_mapping.py",
+        "--obj-dir", f"export/{rel_path.parts[1]}_translated",
+        "--geojson", f"{bo}"
+    ])
+    delete_files(f"export/{rel_path.parts[1]}_translated")
 
-delete_directories(
-    [
-        f"export/{nlp}_{sub_grid}.obj",
-        f"export/{nlp}_{sub_grid}.obj_translated",
-        f"export/{nlp}_{sub_grid}.obj_translated_gml"
-    ]
-)
+    # Convert OBJ ke CityGML lod2
+    sp.call([
+        "go", "run", "obj2lod2gml.go",
+        "-input", f"export/{rel_path.parts[1]}_translated",
+        "-output", f"export/{rel_path.parts[1]}_translated_gml"
+    ])
+    delete_files(f"export/{rel_path.parts[1]}_translated_gml")
+
+    # # Convert OBJ ke CityGML lod1
+    # sp.call([
+    #     "go", "run", "obj2gml.go",
+    #     "-input", f"export/{folder_name}.obj_translated",
+    #     "-output", f"export/{folder_name}.obj_translated_gml"
+    # ])
+
+    # Merge keseluruhan CityGMl lod2 file menjadi 1 file
+    sp.call([
+        "python", "lod2merge.py",
+        f"export/{rel_path.parts[1]}_translated_gml",
+        f"{output_path}",
+        "--name", f"{folder_name}"
+    ])
+
+    # # Merge Keseluruhan CityGML lod1 file menjadi 1 file
+    # sp.call([
+    #     "go", "run", "mergegml.go",
+    #     "-input", f"export/{folder_name}.obj_translated_gml",
+    #     "-output", f"percepatan/citygml/{folder_name}.gml"
+    # ])
+
+    delete_directories(
+        [
+            f"export/{rel_path.parts[1]}",
+            f"export/{rel_path.parts[1]}_translated",
+            f"export/{rel_path.parts[1]}_translated_gml"
+        ]
+    )
 
 end = time.time() - start
 print(f"durasi : {end} s")
